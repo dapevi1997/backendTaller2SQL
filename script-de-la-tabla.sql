cb6327166aafdce9f69ca9dba7fa8fe764c443eb
dapevi1997 <104047714+dapevi1997@users.noreply.github.com>
2022-06-28 12:49:30 -0500

    Carga del script de la base de datos usada

---
+++ script-de-la-tabla.sql
@@ -0,0 +1,21 @@
+-- -----------------------------------------------------
+-- Schema agenda
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS agenda DEFAULT CHARACTER SET utf8 ;
+USE agenda ;
+-- -----------------------------------------------------
+-- Table contact
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS contact (
+  con_id INT NOT NULL AUTO_INCREMENT,
+  con_name VARCHAR(100) NOT NULL,
+  con_number VARCHAR(10) NOT NULL,
+  con_email VARCHAR(70) NOT NULL,
+  con_birthday DATE NOT NULL,
+  PRIMARY KEY (con_id),
+  UNIQUE INDEX con_name_UNIQUE (con_name ASC) VISIBLE,
+  UNIQUE INDEX con_number_UNIQUE (con_number ASC) VISIBLE)
+ENGINE = InnoDB;
+
+alter table contact
+    add deleted boolean default 0 null;
